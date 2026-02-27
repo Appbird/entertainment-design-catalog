6ddc929ad8f416b449fc406a789fb60759aec5e1
Appbird <project.appbird@gmail.com>
2026-02-27 23:28:34 +0900

    ec2025のページとec2026のページを分離

--- vite.config.ts
+++ vite.config.ts
@@ -5,10 +5,17 @@ import react from "@vitejs/plugin-react"
 export default defineConfig(({ command }) => {
 	return {
 		plugins: [react()],
-		publicDir: './source',
+		publicDir: "public",
 		base: "",
 		build: {
-			outDir: 'docs'
+			outDir: 'docs',
+			rollupOptions: {
+				input: {
+					index: resolve(__dirname, "index.html"),
+					ec2025: resolve(__dirname, "ec2025.html"),
+					ec2026si: resolve(__dirname, "ec2026si.html"),
+				},
+			},
 		},
 	}
 })
